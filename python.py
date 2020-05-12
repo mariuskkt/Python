85104efc73db8612054ced46f4c616567dadb6ea
mariuskkt <marius.kukta@gmail.com>
2020-05-12 20:16:00 +0200

    first lecture

--- python.py
+++ python.py
@@ -1,1 +1,47 @@
-print ('hello world')
+# a = int(input('Enter a number: '))
+# b = int(input('Enter another number: '))
+#
+# if a < b:
+#     print('First number is smaller than second')
+# elif a == b:
+#     print('Numbers are equal ')
+# else:
+#     print('First number is bigger than second')
+
+# text = 'Zen of Python'
+# print(text[5])
+# print(text[7])
+# print(text[0:3])
+# print(text[7:13])
+# print(text[::-1])
+# print(text.split())
+# print(text.replace('Zen', 'Programming'))
+
+# a = int(input('Enter a number: '))
+# b = int(input('Enter another number: '))
+# action = int(input('Choose an action: \n'
+#                    '1 for *\n'
+#                    '2 for / \n'
+#                    '3 for + \n'
+#                    '4 for - \n'))
+# if action == 1:
+#     print(a * b)
+# elif action == 2:
+#     print(a / b)
+# elif action == 3:
+#     print(a + b)
+# elif action == 4:
+#     print(a - b)
+# else:
+#     print('You have typed in wrong number')
+
+# a = int(input('Enter a number: '))
+
+# if a % 2 == 0:
+#     print('Skaicius yra lyginis')
+# elif a % 2 != 0:
+#     print('Skaicius yra nelyginis')
+#     if a % 3 == 0:
+#         print('Skaicius dalinasi is 3')
+#     else:
+#         print('Bet nesidalina is 3')
